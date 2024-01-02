e87afdf7bef8d0de43fb611a856ddafa5b4794d3
dmills-MC <155484278+dmills-MC@users.noreply.github.com>
2024-01-02 15:35:54 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("MSAP 2024 - Factory Data Collection")
 
-DataEnteredBy = st.text_input
-Signature = st.text_input
+DataEnteredBy = st.text_input('Enter your name', "default")
+Signature = st.text_input('Sign Here', "default")
 
 st.header("Company Info")
 st.write("Company Name:", '[pre-populate]')
