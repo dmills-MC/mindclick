9bcbff83b86fd549dfe15a3d89a3e9d605744431
dmills-MC <155484278+dmills-MC@users.noreply.github.com>
2024-01-02 12:40:01 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
-st.header(''streamlit_pandas_profiling'')
+st.header('streamlit_pandas_profiling')
 
 df. = pd.read_csv.('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
