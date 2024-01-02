4b9c61a34d61e9e67ad0ccd59358063822e5faac
dmills-MC <155484278+dmills-MC@users.noreply.github.com>
2024-01-02 13:48:00 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,33 @@
 import streamlit as st
-import time
 
-st.title('st.progress')
+st.title('st.form')
 
-with st.expander('About this app'):
-  st.write('You can now display the progress of your calculations in a Streamlit app with the st.progress command.')
+# Example using 'with' notation
 
-  my_bar = st.progress(0)
+st.header('1.Example of using `with` notation')
+st.subheader('Cofee machine')
 
-  for percent_complete in range(100):
-    time.sleep(0.05)
-    my_bar.progress(percent_complete + 1)
+with st.form('my_form'):
+  st.subheader('**Order your coffee**')
 
-  st.balloons()
+ # Input widgets
+    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
+    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
+    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
+    serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
+    milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
+    owncup_val = st.checkbox('Bring own cup')
+
+submitted = st.form_submit_button('Submit')
+
+if submitted:
+  st.markdown(f'''
+    You have ordered:
+    - Coffee bean: `{coffee_bean_val}`
+    - Coffee roast: `{coffee_roast_val}`
+    - Brewing: `{brewing_val}`
+    - Serving type: `{serving_type_val}`
+    - Milk: `{milk_val}`
+    - Bring own cup: `{owncup_val}`
+    ''')
+else st.write('Place your order!')
