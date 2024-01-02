5751d7ed18569c646da89a65e8caad60400a0f09
dmills-MC <155484278+dmills-MC@users.noreply.github.com>
2024-01-02 14:05:20 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ st.write(st.experimental_get_query_params())
 st.header('3. retrieve')
 
 firstname = st.experimental_get_query_params()['firstname'][0]
-surname = st.experimental_get_query_params()['surname][0]
+surname = st.experimental_get_query_params()['surname'][0]
 
 st.write(f'Hello **{firstname} {surname}**')
