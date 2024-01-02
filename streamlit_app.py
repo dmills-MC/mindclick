cc1bac1c5a095a3245e8c1bcccddd7b76b45dfca
dmills-MC <155484278+dmills-MC@users.noreply.github.com>
2024-01-02 15:46:01 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,11 @@ DataEnteredBy = st.text_input('Enter your name', "")
 Signature = st.text_input('Sign Here', "")
 
 st.header("Factory Overview")
+st.subheader("These are the factories we have from last year")
 lst = ['Factory 1', 'Factory 2', 'Factory 3']
-
 for i in lst:
     st.markdown("- " + i)
+
+st.radio("Do you need to remove any of these factories for this year?", ['Yes', 'No'])
+
+st.radio("Do you need to add any new factories for this year?", ['Yes', 'No'])
