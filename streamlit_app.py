f54d0d0cfa09174a4dc962a7e1545020dbf55b1a
dmills-MC <155484278+dmills-MC@users.noreply.github.com>
2024-01-02 15:33:39 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ DataEnteredBy = st.text_input
 Signature = st.text_input
 
 st.header("Company Info")
-    st.write("Company Name:", '[pre-populate]')
-    st.write("Data entered by:", DataEnteredBy)
-    st.write("Signature:", Signature)
+st.write("Company Name:", '[pre-populate]')
+st.write("Data entered by:", DataEnteredBy)
+st.write("Signature:", Signature)
