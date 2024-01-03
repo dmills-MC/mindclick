fb98678d495a542c26aaf6c8ca17384c5c08ff92
dmills-MC <155484278+dmills-MC@users.noreply.github.com>
2024-01-02 16:17:12 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,9 @@ Signature = st.text_input('Sign Here', "")
 
 st.header("Current Factories")
 st.subheader("These are the factories we have from last year")
-st.write("- To remove a factory from this year's assessment - click the checkbox next to the name and select the delete icon")
-st.write("- To add a new factory, enter the name in the last row")
+st.write("- To remove a factory from this year's assessment, click the checkbox next to the name and select the delete icon")
+st.write("- To add a new factory, click the '+' icon in the last row and enter the factory name")
+st.write("- When you are done, click **Submit**")
 current_factories = pd.DataFrame({
     'Factory Name':['Factory 1', 'Factory 2', 'Factory 3']
 })
