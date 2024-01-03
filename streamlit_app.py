94374b5dc1a6e097f27726a08dc7564f05f71e43
dmills-MC <155484278+dmills-MC@users.noreply.github.com>
2024-01-02 16:13:04 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,17 @@ CompanyName = st.text_input('Company Name', '[pre-populate]')
 DataEnteredBy = st.text_input('Enter your name', "")
 Signature = st.text_input('Sign Here', "")
 
-st.header("Factory Overview")
+"""st.header("Factory Overview")
 st.subheader("These are the factories we have from last year")
 lst = ['Factory 1', 'Factory 2', 'Factory 3']
 for i in lst:
     st.markdown("- " + i)
+"""
 
 st.header("Current Factories")
+st.subheader("These are the factories we have from last year")
+st.write("To remove a factory from this year's assessment - click the checkbox next to the name and select the delete icon",
+         "To add a new factory, enter the name in the last row")
 current_factories = pd.DataFrame({
     'Factory Name':['Factory 1', 'Factory 2', 'Factory 3']
 })
@@ -24,8 +28,7 @@ st.data_editor(current_factories,
     num_rows="dynamic"
     )
 
-
-
+"""
 remove_factories = st.radio("Do you need to remove any of these factories for this year?", ['Yes', 'No'])
 if remove_factories == 'Yes':
     st.text_input('Factory Name')
@@ -33,3 +36,4 @@ else:
     st.write()
 
 add_factories = st.radio("Do you need to add any new factories for this year?", ['Yes', 'No'])
+"""
