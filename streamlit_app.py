3e55c05213d7bc22707b11be8b02f40e06da3a09
dmills-MC <155484278+dmills-MC@users.noreply.github.com>
2024-01-02 16:03:11 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("MSAP 2024 - Factory Data Collection")
 
@@ -13,14 +14,18 @@ lst = ['Factory 1', 'Factory 2', 'Factory 3']
 for i in lst:
     st.markdown("- " + i)
 
+st.header("Current Factories")
+pd.DataFrame({
+    'Factory Name':['Factory 1', 'Factory 2', 'Factory 3']
+    num_rows="dynamic"
+})
+
+
+
 remove_factories = st.radio("Do you need to remove any of these factories for this year?", ['Yes', 'No'])
 if remove_factories == 'Yes':
     st.text_input('Factory Name')
 else:
     st.write()
-    
+
 add_factories = st.radio("Do you need to add any new factories for this year?", ['Yes', 'No'])
-if add_factories == 'Yes':
-    st.text_input('Factory Name')
-else:
-    st.write()
